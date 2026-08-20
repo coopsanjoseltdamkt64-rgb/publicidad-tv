4ad78ee7878a3d6260fd8d19809e58407656ac4c
coopsanjoseltdamkt64-rgb <coopsanjoseltdamkt64@gmail.com>
2026-08-20 14:04:07 -0500

    Add files via upload

---
+++ configuracion.js
@@ -0,0 +1,12 @@
+window.TV_CONFIG={
+  refreshMinutes:5,
+  imageDuration:15,
+  playlist:[
+    // Ejemplos:
+    // {src:'contenido/panapay.mp4'},
+    // {src:'contenido/bioverde.jpg',duration:15},
+    // {src:'contenido/credito-flash.mp4'},
+    // {src:'contenido/san-jose-movil.png',duration:15}
+  ]
+  // Alternativamente usa: playlistUrl:'playlist.json'
+};
